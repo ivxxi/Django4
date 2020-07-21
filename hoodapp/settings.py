811c8fe3d4b73148103f65e755501af18005bc18
ivxxi <crystalalice21@gmail.com>
2020-07-21 11:47:17 +0300

    removes local db

--- hoodapp/settings.py
+++ hoodapp/settings.py
@@ -113,14 +113,14 @@ WSGI_APPLICATION = 'hoodapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'neighborhood',
-        'USER': 'crystal',
-        'PASSWORD': '252525'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'neighborhood',
+#         'USER': 'crystal',
+#         'PASSWORD': '252525'
+#     }
+# }
 
 
 # Password validation
